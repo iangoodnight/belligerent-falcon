700a1d7817b6cd72f2df8f7932562a494ba6cd81
root <jonathon.bores@bulkapothecary.com>
2019-05-16 16:15:39 -0400

    Themeing should be finished and polished at this point

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-var app = require('express')();
-var http = require('http').Server(app);
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var data = require('./scrape.js');
+var path = require('path');
 
 var newScrape = data.scrape();
+
+app.use(express.static(path.join(__dirname, 'public'))); // Dictates that root directory from which to serve static assets.
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3001, function() {
-	console.log('HTTP server started on port 3001');
-});
-
 io.on('connection', function(socket) {
 	console.log('Client connection received');
+
+	socket.on('disconnect', function() {
+		console.log('user disconnected');
+	});
 
 	socket.emit('data', newScrape);
 	console.log("new scrape: " + JSON.stringify(newScrape));
@@ -22,13 +26,30 @@ io.on('connection', function(socket) {
 	socket.on('receivedFromClient', function(data) {
 		console.log(data);
 	});
+
+	setInterval(function() {
+		newScrape = data.scrape();
+
+		socket.emit('newData', newScrape);
+
+		console.log('Last updated: ' + new Date());
+		console.log('interval scrape: ' + JSON.stringify(newScrape));
+
+	}, 75000);
+
 }); 
 
-setInterval(function() {
-	newScrape = data.scrape();
+http.listen(3001, function() {
+	console.log('HTTP server started on port 3001');
+});
 
-	io.emit('data', newScrape);
 
-	console.log('Last updated: ' + new Date());
+// setInterval(function() {
+// 	newScrape = data.scrape();
 
-}, 75000);
+// 	io.emit('newData', newScrape);
+
+// 	console.log('Last updated: ' + new Date());
+// 	console.log('interval scrape: ' + newScrape);
+
+// }, 75000);

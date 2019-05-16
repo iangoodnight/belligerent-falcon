1b0a2a1a79bcc9fc75eb824f853322b45b0d3f1e
Ian Goodnight <Ian.Goodnight@bulkapothecary.com>
2019-05-16 17:08:44 -0400

    removed old code to simplify merging

--- scrape.js
+++ scrape.js
@@ -11,9 +11,9 @@ var scrape = function() {
 
 	//  Firstly, tell the console what scrape.js is doing
 	console.log("\n***************************************\n" +
-							"Grabbing names and statuses\n" + 
-							"from 'http://status.bigcommerce.com/'" +
-							"\n***************************************\n");
+				"Grabbing names and statuses\n" + 
+				"from 'http://status.bigcommerce.com/'" +
+				"\n***************************************\n");
 
 	//  Making a request from BigCommerce's status page.  The pages HTML is passed as the callback's third argument
 	request(url, function(error, response, html) {
@@ -21,9 +21,6 @@ var scrape = function() {
 		//  Load the HTML into cheerio and save it as a variable.
 		//  '$' becomes shorthand for cheerio's slector commands, much like jQuery's '$'
 		var $ = cheerio.load(html);
-
-		// //  An empty array to save the data that we'll scrape.
-		// var results = [];
 
 		//  With cheerio, find each div tag with the 'component-inner-container' class.
 		// (i: iterator, element: the current element)
@@ -53,54 +50,5 @@ var scrape = function() {
 };
 
 exports.scrape = scrape;
-
-// var options = {
-// 	uri: url,
-// 	transform: function (body) {
-// 		return cheerio.load(body);
-// 	}
-// };
-
-// //  Expose our 'scrape' function to be imported by our server.
-// exports.scrape = function() { 
-
-// 	//  An empty array to save the data that we'll scrape.
-// 	var results = [];
-
-// 	rp(options)
-// 		.then(function ($) {
-
-
-// 		//  Firstly, tell the console what scrape.js is doing
-// 		console.log("\n***************************************\n" +
-// 								"Grabbing names and statuses\n" + 
-// 								"from 'http://status.bigcommerce.com/'" +
-// 								"\n***************************************\n");
-// 		//  With cheerio, find each div tag with the 'component-inner-container' class.
-// 		// (i: iterator, element: the current element)
-// 		$("div.component-inner-container").each(function(i, element) {
-// 			//  Declare our name and status variables for building our object.
-// 			var name, status;
-// 			//  Pull out the names from our span tags.
-// 			name = $(element).children('.name').text().trim();
-// 			//  Pull out the statuses from our span tags.
-// 			status = $(element).children('.component-status').text().trim();
-// 			//  Print results to the console (for testing)
-// 			console.log("name: " + name);
-// 			console.log("status: " + status  + "\n========================");
-// 			//  Save results in an object that we'll push into the results array we defined earlier.
-// 			results.push({
-// 				name: name,
-// 				status: status
-// 			});
-// 		});
-
-// 	})
-// 	.catch(function (err) {
-// 		console.log("err: " + err);
-// 	});
-// 	//  return our scraped data.
-// 	return results;
-// };
 
 

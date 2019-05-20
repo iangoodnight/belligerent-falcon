770d0cb74b125b9c5a3b2a449a7c77f3a306623b
Ian Goodnight <goodnight.ian@gmail.com>
2019-05-20 00:03:07 -0400

    promise-based scrape: first draft

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-var data = require('./scrape.js');
+// var data = require('./scrape.js');
 var path = require('path');
-
-var newScrape = data.scrape();
+const rp = require('request-promise');
+const request = require('request');
+const url = 'https://status.bigcommerce.com/';
+const cheerio = require('cheerio');
 
 app.use(express.static(path.join(__dirname, 'public'))); // Dictates that root directory from which to serve static assets.
 
@@ -13,43 +15,160 @@ app.get('/', function(req, res) {
 	res.sendFile(__dirname + './public/index.html');
 });
 
-io.on('connection', function(socket) {
-	console.log('Client connection received');
+let interval;
+
+io.on('connection', socket => {
+	console.log('New client connection received');
+	if (interval) {
+		clearInterval(interval);
+	}
+	interval = setInterval(() => getBCAndEmit(socket)
+		.then(getSSAndEmit(socket))
+		.then(getHSAndEmit(socket))
+		.then(getAlvAndEmit(socket)), 10000);
 
 	socket.on('disconnect', function() {
-		console.log('user disconnected');
+		console.log('Client disconnected');
 	});
-
-	socket.emit('data', newScrape);
-	console.log("new scrape: " + JSON.stringify(newScrape));
-
-	socket.on('receivedFromClient', function(data) {
-		console.log(data);
-	});
-
-	setInterval(function() {
-		newScrape = data.scrape();
-
-		socket.emit('newData', newScrape);
-
-		console.log('Last updated: ' + new Date());
-		console.log('interval scrape: ' + JSON.stringify(newScrape));
-
-	}, 75000);
 
 }); 
+
+const getBCAndEmit = async socket => {
+	try {
+		const res =  await request(url, function(error, response, html) {
+			var $ = cheerio.load(html);
+			// (i: iterator, element: the current element)
+			let results = {'bigCommerce':[]};
+			$("div.component-inner-container").each(function(i, element) {
+				//  Declare our name and status variables for building our object.
+				var name, status;
+				//  Pull out the names from our span tags.
+				name = $(element).children('.name').text().trim();
+				//  Pull out the statuses from our span tags.
+				status = $(element).children('.component-status').text().trim();
+				//  Print results to the console (for testing)
+				console.log("name: " + name);
+				console.log("status: " + status  + "\n========================");
+				//  Save results in an object that we'll push into the results array we defined earlier.
+				results.bigCommerce.push({
+					name: name,
+					status: status
+				});
+			});
+			console.log("results: " + JSON.stringify(results));
+			response = results;
+			// return results;
+			socket.emit('newData', results);
+		});
+
+	} catch (error) {
+		console.error(`Error: ${error.code}`);
+	}
+};
+
+const getSSAndEmit = async socket => {
+	try {
+		const res =  await request("http://status.shipstation.com", function(error, response, html) {
+			var $ = cheerio.load(html);
+			// (i: iterator, element: the current element)
+			// let SSStatuses = [];
+			let results = {'shipStation': []};
+			$("div.component-inner-container").each(function(i, element) {
+				//  Declare our name and status variables for building our object.
+				var name, status;
+				//  Pull out the names from our span tags.
+				name = $(element).children('.name').text().trim();
+				//  Pull out the statuses from our span tags.
+				status = $(element).children('.component-status').text().trim();
+				//  Print results to the console (for testing)
+				console.log("name: " + name);
+				console.log("status: " + status  + "\n========================");
+				//  Save results in an object that we'll push into the results array we defined earlier.
+				results.shipStation.push({
+					name: name,
+					status: status
+				});
+			});
+			console.log("results: " + JSON.stringify(results));
+			response = results;
+			// return results;
+			socket.emit('newData', results);
+		});
+
+	} catch (error) {
+		console.error(`Error: ${error.code}`);
+	}
+};
+
+const getHSAndEmit = async socket => {
+	try {
+		const res =  await request("http://status.shipstation.com", function(error, response, html) {
+			var $ = cheerio.load(html);
+			// (i: iterator, element: the current element)
+			// let SSStatuses = [];
+			let results = {'helpScout': []};
+			$("div.component-inner-container").each(function(i, element) {
+				//  Declare our name and status variables for building our object.
+				var name, status;
+				//  Pull out the names from our span tags.
+				name = $(element).children('.name').text().trim();
+				//  Pull out the statuses from our span tags.
+				status = $(element).children('.component-status').text().trim();
+				//  Print results to the console (for testing)
+				console.log("name: " + name);
+				console.log("status: " + status  + "\n========================");
+				//  Save results in an object that we'll push into the results array we defined earlier.
+				results.helpScout.push({
+					name: name,
+					status: status
+				});
+			});
+			console.log("results: " + JSON.stringify(results));
+			response = results;
+			// return results;
+			socket.emit('newData', results);
+		});
+
+	} catch (error) {
+		console.error(`Error: ${error.code}`);
+	}
+};
+
+const getAlvAndEmit = async socket => {
+	try {
+		const res =  await request("https://status.avalara.com/", function(error, response, html) {
+			var $ = cheerio.load(html);
+			// (i: iterator, element: the current element)
+			// let SSStatuses = [];
+			let results = {'alvara': []};
+			$("div.component-inner-container").each(function(i, element) {
+				//  Declare our name and status variables for building our object.
+				var name, status;
+				//  Pull out the names from our span tags.
+				name = $(element).children('.name').text().trim();
+				//  Pull out the statuses from our span tags.
+				status = $(element).children('.component-status').text().trim();
+				//  Print results to the console (for testing)
+				console.log("name: " + name);
+				console.log("status: " + status  + "\n========================");
+				//  Save results in an object that we'll push into the results array we defined earlier.
+				results.alvara.push({
+					name: name,
+					status: status
+				});
+			});
+			console.log("results: " + JSON.stringify(results));
+			response = results;
+			// return results;
+			socket.emit('newData', results);
+		});
+
+	} catch (error) {
+		console.error(`Error: ${error.code}`);
+	}
+};
+
 
 http.listen(3001, function() {
 	console.log('HTTP server started on port 3001');
 });
-
-
-// setInterval(function() {
-// 	newScrape = data.scrape();
-
-// 	io.emit('newData', newScrape);
-
-// 	console.log('Last updated: ' + new Date());
-// 	console.log('interval scrape: ' + newScrape);
-
-// }, 75000);
--- public/Javascripts/status.js
+++ public/Javascripts/status.js
@@ -11,7 +11,46 @@ socket.on('data', function(data) {
 });
 socket.on('newData', function(data) {
 	console.log(data);
-	var results = "";
+	for (var key in data) {
+		var results = "";
+		console.log("key: " + key)
+		switch (key) {
+			case ('shipStation'):
+				console.log("case 1");
+				for (var i = 0; i < data.shipStation.length; i++) {
+					results = results + "<p>" + data.shipStation[i].name + ": " + data.shipStation[i].status + "</p>";
+				}
+				console.log("case 1 results: " + results);
+				$("#ship-station").html(results);
+				break;
+			case ('bigCommerce'):
+				console.log("case 2");
+				for (var i = 0; i < data.bigCommerce.length; i++) {
+					results = results + "<p>" + data.bigCommerce[i].name + ": " + data.bigCommerce[i].status + "</p>";
+				}
+				$("#big-commerce").html(results);
+				break;
+			case ('helpScout'):
+			console.log("case 3");
+				for (var i = 0; i < data.helpScout.length; i++) {
+					results = results + "<p>" + data.helpScout[i].name + ": " + data.helpScout[i].status + "</p>";
+				}
+				$("#help-scout").html(results);
+				break;
+			case ('alvara'):
+			console.log("case 4");
+				for (var i = 0; i < data.alvara.length; i++) {
+					results = results + "<p>" + data.alvara[i].name + ": " + data.alvara[i].status + "</p>";
+				}
+				$("#alvara").html(results);
+				break;
+			default:
+				break;
+		}
+	}
+	// switch (data) {
+	// 	case data[0]
+	// }
 	for (var i = 0; i < data.length; i++) {
 		results = results + "<p>" + data[i].name + ": " + data[i].status + "</p>";
 	}
